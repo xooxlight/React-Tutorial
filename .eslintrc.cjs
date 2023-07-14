7955dcb683eebe2ddc339bf5be2dad2730c76979
xooxlight <xooxlight@gmail.com>
2023-07-14 20:16:15 +0900

    Setting ESLint, Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,35 @@
-/* eslint-env node */
-
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'no-unused-vars': 'error',
+    'no-multiple-empty-lines': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
